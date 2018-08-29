84e6dd490db0f3a63d7034e2f002f40041f29dca
Justin Bull <bull.justin@gmail.com>
2018-08-28 20:53:54 -0700

    Auto suggest pokemon when form input changes

--- src/app/components/Rolodex/index.tsx
+++ src/app/components/Rolodex/index.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
+import * as Autosuggest from 'react-autosuggest';
 
 export namespace Rolodex {
     export interface Props {
     }
+
     export interface State {
         error?: Error,
         isLoaded: boolean,
-        pokedex?: Pokedex
+        pokedex?: Pokedex,
+        searchValue: string,
+        suggestions: Suggestion[]
     }
 }
 
@@ -15,45 +19,104 @@ export class Rolodex extends React.Component<Rolodex.Props, Rolodex.State> {
     constructor(props: Rolodex.Props) {
         super(props);
         this.state = {
-            error: undefined,
             isLoaded: false,
-            pokedex: undefined
+            searchValue: '',
+            suggestions: []
         };
     }
 
-    fetchPokedex() {
+    onChange = (event: React.FormEvent<HTMLInputElement>) => {
+        this.setState({
+            searchValue: event.currentTarget.value
+        });
+    };
+
+    onSuggestionsFetchRequested = (request: Autosuggest.SuggestionsFetchRequestedParams): void => {
+        this.setState({
+            suggestions: this.getSuggestions(request.value)
+        });
+    };
+
+    onSuggestionsClearRequested = (): void => {
+        this.setState({
+            suggestions: []
+        });
+    };
+
+    getSuggestions = (value: string): Array<Suggestion> => {
+        const inputValue = value.trim().toLowerCase();
+        const inputLength = inputValue.length;
+
+        let suggestions: Array<Suggestion> = [];
+
+        if (this.state.pokedex) {
+            this.state.pokedex.pokemon_entries.forEach((object: PokedexPokemonEntry) => {
+                if (object.pokemon_species.name.toLowerCase().slice(0, inputLength) === inputValue) {
+                    let suggestion: Suggestion = {name: object.pokemon_species.name};
+                    suggestions.push(suggestion);
+                }
+            });
+        }
+
+        return suggestions;
+    };
+
+    getSuggestionValue = (suggestion: Suggestion) => suggestion.name;
+
+    renderSuggestion = (suggestion: Suggestion) => (
+        <div>
+            {suggestion.name}
+        </div>
+    );
+
+    fetchPokedex(): void {
         fetch('https://pokeapi.co/api/v2/pokedex/1')
             .then(
-            (response: Response) => {
-                response.json().then((data) => {
-                    console.log(data);
+                (response: Response) => {
+                    response.json().then((data) => {
+                        this.setState({
+                            isLoaded: true,
+                            pokedex: data
+                        });
+                    });
+                },
+                (error: Error) => {
                     this.setState({
                         isLoaded: true,
-                        pokedex: data
+                        error: error
                     });
-                });
-            },
-            (error: Error) => {
-                this.setState({
-                    isLoaded: true,
-                    error: error
-                });
-            }
-        )
+                }
+            )
     }
 
-    componentDidMount() {
+    componentDidMount(): void {
         this.fetchPokedex();
     }
 
     render() {
+
+        const inputProps: Autosuggest.InputProps<Suggestion> = {
+            placeholder: 'Search for a pokemon...',
+            value: this.state.searchValue,
+            onChange: this.onChange
+        };
+
         if (this.state.error) {
             return <div>Error: {this.state.error.message}</div>;
         } else if (!this.state.isLoaded) {
             return <div>Loading...</div>;
         } else if (this.state.isLoaded && this.state.pokedex) {
             return (
-                <ol>{this.state.pokedex.pokemon_entries.map((object) => <li>{object.pokemon_species.name}</li>)}</ol>
+                <div>
+                    <Autosuggest
+                        suggestions={this.state.suggestions}
+                        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                        getSuggestionValue={this.getSuggestionValue}
+                        renderSuggestion={this.renderSuggestion}
+                        inputProps={inputProps}
+                    />
+                </div>
             );
         } else {
             return <div>Something went wrong...</div>
---
+++ types/suggestion.d.ts
@@ -0,0 +1,3 @@
+interface Suggestion {
+  name: string
+}
